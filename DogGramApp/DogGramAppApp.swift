ee27187c1b1d86c2ce1df53ce945a77cfd10fca6
vickyzhang7 <weiqi997998@gmail.com>
2023-12-11 17:55:04 -0500

    Initial Commit

---
+++ DogGramApp/DogGramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramAppApp.swift
+//  DogGramApp
+//
+//  Created by Weiqi on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DogGramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
